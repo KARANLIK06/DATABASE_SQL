f4618f9b7bcd99c3918d38c2dfafda2212f20f8f
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-22 23:17:55 +0300

    Day04_SQL

--- Queries/day04/set_operators.sql
+++ Queries/day04/set_operators.sql
@@ -1,4 +1,53 @@
-select *
-from DEVELOPERS;
-select *
-from TESTERS;
+select * from DEVELOPERS
+union all
+select * from TESTERS;
+
+select * from TESTERS
+union all
+select * from DEVELOPERS;
+
+
+
+select * from DEVELOPERS
+union
+select * from TESTERS;
+
+select * from TESTERS
+union
+select * from DEVELOPERS;
+
+
+
+select NAMES from DEVELOPERS
+union
+select NAMES from TESTERS;
+
+select NAMES from TESTERS
+union
+select NAMES from DEVELOPERS;
+
+
+
+select NAMES from DEVELOPERS
+minus
+select NAMES from TESTERS;
+
+select NAMES from TESTERS
+minus
+select NAMES from DEVELOPERS;
+
+
+
+select NAMES from DEVELOPERS
+intersect
+select NAMES from TESTERS;
+
+select NAMES from TESTERS
+intersect
+select NAMES from DEVELOPERS;
+
+--how to find duplicate names in employees table
+select  FIRST_NAME,count(*)
+from EMPLOYEES
+group by FIRST_NAME
+having count(*)>1;
